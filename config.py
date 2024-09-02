d723d2e4a7e9dc4f0cba2ca99c69d0397060d6ed
derek <derek.nirenberg@gmail.com>
2024-09-02 12:04:12 -0400

    Added comments to config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 from pynput import keyboard
 import main
 
+# pair character keys with sound files here, in the format 'h': 'filename.mp3'
 soundboard = {
     '1': 'America the Beautiful.mp3',
     '2': 'Crossing the Bar.mp3',
@@ -8,12 +9,13 @@ soundboard = {
     '4': 'Hallelujah.mp3'
             }
 
+# info for the counter function
 counter_message = 'Your counter message goes here'
 counter_start_number = 0
 counter_increase_key = '5'
 counter_decrease_key = '6'
 
-
+# click the green arrow to run
 if __name__ == '__main__':
     print('starting...')
     main.start_counter(counter_message, counter_start_number)
